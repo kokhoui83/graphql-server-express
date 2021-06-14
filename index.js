baee7df60ef1852034f1954a6a8f3620fb0c436f
brian lim <kokhoui.lim@duerrdental.com>
2021-06-14 17:47:51 +0800

    add query and mutation

--- index.js
+++ index.js
@@ -1,14 +1,84 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { makeExecutableSchema } from 'graphql-tools'
+
+const typeDefs = `
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+
+  input BookInput {
+    title: String
+    author: String
+  }
+
+  type Mutation {
+    createBook(input: BookInput): Book
+  }
+`
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+  Mutation: {
+    createBook: (_, { input }) => {
+      const newbook = {
+        author: input.author,
+        title: input.title
+      }
+
+      books.push(newbook)
+
+      return newbook
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
 
 const app = express();
+
+const host = '0.0.0.0'
+const port = 5000
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: MyGraphQLSchema,
+    schema,
     graphiql: true,
   }),
 );
 
-app.listen(4000);
+app.listen(port, host, error => {
+  if (error) {
+      process.exit(1)
+  }
+
+  console.log(`Running at ${host}:${port}`)
+})
